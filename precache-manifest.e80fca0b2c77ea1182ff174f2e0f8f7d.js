b459b84e3390a9a8aad3bbab1159c48e163476ee
Galzar07 <52508198+Galzar07@users.noreply.github.com>
2020-05-28 12:04:58 +0200

    Updates

---
+++ precache-manifest.e80fca0b2c77ea1182ff174f2e0f8f7d.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0021193ccc5f33e889abbb2788a84c53",
+    "url": "/Simple-App-React-Router/index.html"
+  },
+  {
+    "revision": "a743fc1658737a4bde83",
+    "url": "/Simple-App-React-Router/static/css/main.575a1174.chunk.css"
+  },
+  {
+    "revision": "6abe4ae5dd17a9ecf462",
+    "url": "/Simple-App-React-Router/static/js/2.57ee3617.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Simple-App-React-Router/static/js/2.57ee3617.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a743fc1658737a4bde83",
+    "url": "/Simple-App-React-Router/static/js/main.beaddaa6.chunk.js"
+  },
+  {
+    "revision": "93afc366b126ad306cff",
+    "url": "/Simple-App-React-Router/static/js/runtime-main.5e004675.js"
+  },
+  {
+    "revision": "70e6670ee746fc59f414fde4fcce93af",
+    "url": "/Simple-App-React-Router/static/media/header1.70e6670e.jpg"
+  },
+  {
+    "revision": "33eca35fceea8311d67358a6629fa9d9",
+    "url": "/Simple-App-React-Router/static/media/header2.33eca35f.jpg"
+  },
+  {
+    "revision": "d9d36b65a132d26aed884ad58e4719eb",
+    "url": "/Simple-App-React-Router/static/media/header3.d9d36b65.jpg"
+  }
+]);
